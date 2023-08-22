0148bbad9a95a0f8a5509a271fe5d7706707b7e4
Tomasz Cekało <tomasz.cekalo@gmail.com>
2023-08-22 08:53:39 +0200

    update nugets Increase version add tests

---
+++ Undine.MonoGame.Extended.Entities.Tests/EntityTests.cs
@@ -0,0 +1,46 @@
+﻿using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Undine.Core;
+using Undine.MonoGame.Extended.Tests.Components;
+
+namespace Undine.MonoGame.Extended.Entities.Tests
+{
+    [TestClass]
+    public class EntityTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+        }
+
+        [TestMethod]
+        public void ComponentCanBeAdded()
+        {
+            var container = new MGEContainer();
+            var mock = Substitute.For<UnifiedSystem<AComponent>>();
+            container.AddSystem(mock);
+            container.Init();
+            var entity = container.CreateNewEntity();
+            entity.AddComponent(new AComponent());
+        }
+
+        [TestMethod]
+        public void ComponentCanBeRetrieved()
+        {
+            var container = new MGEContainer();
+            var mock = Substitute.For<UnifiedSystem<AComponent>>();
+            container.AddSystem(mock);
+            container.Init();
+            var entity = (MGEEntity)container.CreateNewEntity();
+            entity.AddComponent(new AComponent());
+
+            ref var component = ref entity.GetComponent<AComponent>();
+            Assert.IsNotNull(component);
+        }//
+    }
+}
